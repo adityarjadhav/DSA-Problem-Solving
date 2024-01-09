113675144656e457753c8f18026c5ea3260f373b
adityarjadhav <adityarjadhav@outlook.com>
2024-01-09 18:31:03 +0530

    added linear_serach

---
+++ Algorithms/searching_alorithms/linear_search.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int search(int arr[], int N, int x) {
+    for (int i = 0; i < N; i++) {
+        if (arr[i] == x) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(void) {
+    int arr[] = {2, 3, 4, 10, 40};
+    int x = 10;
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    int result = search(arr, N, x);
+    if (result == -1) {
+        printf("Element not found in array\n");
+    } else {
+        printf("Element found at index %d\n", result);
+    }
+
+    return 0;
+}
+
