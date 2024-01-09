ccc18195fdcc8a62fe314df203056e3f21d2ac27
adityarjadhav <adityarjadhav@outlook.com>
2024-01-09 18:18:05 +0530

    updated binary_search.cpp

--- Algorithms/binary_search.cpp
+++ Algorithms/binary_search.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Recursive binary search function
 int binarySearch (int arr[], int l, int r, int x) {
     if (r >= l) {
         int mid = l + (r - l) / 2;
@@ -15,19 +14,18 @@ int binarySearch (int arr[], int l, int r, int x) {
         return binarySearch(arr, mid + 1, r, x);
     }
 
-    return -1; // Element not found
+    return -1; 
 }
 
-// Driver code
 int main() {
     int arr[] = {2, 3, 4, 10, 40};
     int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    // Call binarySearch function
+   
     int result = binarySearch(arr, 0, n - 1, x);
 
-    // Display the result
+   
     if (result == -1) {
         cout << "Element not found";
     } else {
