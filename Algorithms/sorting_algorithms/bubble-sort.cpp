a0bbc36b765b1098721417a110ca72df4e06a5db
adityarjadhav <adityarjadhav@outlook.com>
2024-01-12 11:54:54 +0530

    added bubble-sort

---
+++ Algorithms/sorting_algorithms/bubble-sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // Call Bubble Sort function
+    bubbleSort(arr, n);
+
+    cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
