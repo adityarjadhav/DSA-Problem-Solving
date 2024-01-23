56d4a96fba7ad58d3faa78eceac5172336f3b39d
adityarjadhav <adityarjadhav@outlook.com>
2024-01-23 19:46:24 +0530

    Stack push and pop operation

---
+++ Data_structures/stack/StackPushandPop.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(Stack *ptr)
+{
+    return ptr->top == -1;
+}
+
+int isFull(Stack *ptr)
+{
+    return ptr->top == ptr->size - 1;
+}
+
+void push(Stack *ptr, int value)
+{
+    if (isFull(ptr))
+    {
+        cout << "Stack Overflow! Cannot push " << value << ".\n";
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+        cout << value << " pushed to the stack.\n";
+    }
+}
+
+int main()
+{
+    Stack *s = new Stack;
+    s->size = 5;
+    s->top = -1;
+    s->arr = new int[s->size];
+
+    // Pushing elements onto the stack
+    push(s, 20);
+    push(s, 34);
+
+    // Your stack now contains 20 and 34
+
+    // Free allocated memory
+    delete[] s->arr;
+    delete s;
+
+    return 0;
+}
